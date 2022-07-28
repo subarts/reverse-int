0996323d36a84cbd2b9fb51512341ed515500a17
artem subbotin <subbota1991@gmail.com>
2022-07-28 19:13:51 +0400

    start: task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports =function reverse (n) {
+    let num=n
+    if(num<0){
+         num= n*-1
+    }  
+  let arr = ('' + num).split('')
+  arr.reverse()
+    let arr2=arr.join('')
+    return arr2
 }
